e03b48665096daa04e96495a3c3cae4ba80ea84d
Matt Jones <mattjones@ebsco.com>
2018-01-25 14:41:43 -0500

    Add missing resolve paths for when stripes-core is aliased, follow-up to STCLI-13

--- test/webpack-common.spec.js
+++ test/webpack-common.spec.js
@@ -41,6 +41,14 @@ describe('The webpack-common module', function () {
       expect(result).to.have.property('entry').that.does.not.include('/original/path/to/@folio/stripes-core/src/index');
     });
 
+    it('updates resolve modules for an aliased stripes-core', function () {
+      const webpackOverride = this.sut.cliResolve(true, '/alias/path/to/@folio/stripes-core');
+      const result = webpackOverride(this.inputConfig);
+
+      expect(result.resolve.modules).to.include('/alias/path/to/@folio/stripes-core/node_modules');
+      expect(result.resolveLoader.modules).to.include('/alias/path/to/@folio/stripes-core/node_modules');
+    });
+
     it('updates entry for the platform\'s stripes-core (platform core available)', function () {
       const webpackOverride = this.sut.cliResolve(true, '');
       const result = webpackOverride(this.inputConfig);
