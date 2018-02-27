2514c9317eb0f844e584b5aaffe1bc3b4546a527
Matt Jones <mattjones@ebsco.com>
2018-02-27 15:39:42 -0500

    Add comments and rename placeholder config

--- resources/workspace/.stripesclirc.js
+++ resources/workspace/.stripesclirc.js
@@ -8,4 +8,4 @@
   If you want to share aliases across multiple platforms in the same workspace, then modify this file and remove your platform's .stripesclirc.
   Note: doing so will prevent independent serving apps, unless the aliases are manually commented out (or an empty .stripesclirc exists within the app directory).
 */
-{}
+module.exports = {};
