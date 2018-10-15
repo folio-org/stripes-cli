cf5533c08e55dc9323c035ea858febd7023c9f2d
Wil Wilsman <wil@frontside.io>
2018-10-15 16:06:27 -0500

    Clean up bigtest resources

--- resources/ui-app/test/bigtest/interactors/application.js
+++ resources/ui-app/test/bigtest/interactors/application.js
@@ -4,6 +4,7 @@ import {
   property
 } from '@bigtest/interactor';
 
+// https://bigtestjs.io/guides/interactors/introduction/
 export default @interactor class ApplicationInteractor {
   greetingMessage = text('[data-test-application-greeting]');
   exampleLink = property('[data-test-application-examples] a', 'href');
