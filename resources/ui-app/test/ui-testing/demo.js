4bcd1bd5447d1bfa460ef0c773178a5b0e5875d5
Wil Wilsman <wil@frontside.io>
2018-10-17 09:59:08 -0500

    Add back nightmare tests to ui-app blueprint

---
+++ resources/ui-app/test/ui-testing/demo.js
@@ -0,0 +1,49 @@
+/* global Nightmare, describe, it, before, after */
+
+module.exports.test = (uiTestCtx) => {
+  describe('Module test: <%= uiAppName %>:', () => {
+    const { config, helpers: { login, logout } } = uiTestCtx;
+    const nightmare = new Nightmare(config.nightmare);
+
+    this.timeout(Number(config.test_timeout));
+
+    describe('Login > navigate to app > verify message > logout', () => {
+      before((done) => {
+        login(nightmare, config, done);
+      });
+      after((done) => {
+        logout(nightmare, config, done);
+      });
+      it('should open app and see stripes-new-app-greeting', (done) => {
+        nightmare
+          .wait('#clickable-<%= appName %>-module')
+          .click('#clickable-<%= appName %>-module')
+          .wait('#<%= appName %>-module-display')
+          .wait('[data-test-application-greeting]')
+          .then(() => { done(); })
+          .catch(done);
+      });
+    });
+
+    describe('Login > navigate to app settings > verify message > logout', () => {
+      before((done) => {
+        login(nightmare, config, done);
+      });
+      after((done) => {
+        logout(nightmare, config, done);
+      });
+      it('should open app settings and see stripes-new-app-settings-message', (done) => {
+        nightmare
+          .wait(config.select.settings)
+          .click(config.select.settings)
+          .wait('a[href="/settings<%= appRoute %>"]')
+          .click('a[href="/settings<%= appRoute %>"]')
+          .wait('a[href="/settings<%= appRoute %>/general"]')
+          .click('a[href="/settings<%= appRoute %>/general"]')
+          .wait('[data-test-application-settings-general-message]')
+          .then(() => { done(); })
+          .catch(done);
+      });
+    });
+  });
+};
