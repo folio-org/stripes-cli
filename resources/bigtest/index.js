4cd7be1ab99f197dfdc197494c8633b67be0110e
Ryan Berger <rberger@ebsco.com>
2019-07-25 16:11:55 -0400

    Removing /resources directory as this was all dedicated to the app template

--- resources/bigtest/index.js
+++
@@ -1,6 +0,0 @@
-import 'babel-polyfill';
-
-// require all modules ending in "-test" from the current directory and
-// all subdirectories
-const requireTest = require.context('./tests/', true, /-test/);
-requireTest.keys().forEach(requireTest);
--- resources/bigtest/interactors/application.js
+++
@@ -1,6 +0,0 @@
-import { interactor } from '@bigtest/interactor';
-
-// https://bigtestjs.io/guides/interactors/introduction/
-export default @interactor class ApplicationInteractor {
-  static defaultScope = '#ModuleContainer';
-}
