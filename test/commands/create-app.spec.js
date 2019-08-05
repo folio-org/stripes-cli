492a0e708ad1d7f45a6b18f4a5bc5492ba02bfb1
Ryan Berger <rberger@ebsco.com>
2019-08-05 14:36:58 -0400

    Increased coverage to 98%

--- test/commands/create-app.spec.js
+++ test/commands/create-app.spec.js
@@ -9,11 +9,11 @@ const createApp = require('../../lib/create-app');
 describe('The app create command', function () {
   beforeEach(function () {
     this.sandbox.stub(path, 'sep').returns('/');
-    this.sandbox.stub(fs, 'readFileSync').returns('');
+    this.sandbox.stub(fs, 'readFileSync').returns('__appDescription__');
     this.sandbox.stub(fs, 'writeFileSync');
     this.sandbox.stub(fs, 'removeSync');
-    this.sandbox.stub(fs, 'readdirSync').returns(['__appName__', '__appDescription__']);
-    this.sandbox.stub(fs, 'statSync').returns({ isDirectory() { return false; } });
+    this.sandbox.stub(fs, 'renameSync');
+    this.sandbox.stub(fs, 'readdirSync').returns(['__appDir__', '__appName__.js']);
     this.sandbox.stub(simpleGit.prototype, '_run').callsFake(function (command, cb) {
       console.log('called command', command);
 
@@ -26,10 +26,16 @@ describe('The app create command', function () {
       return this;
     });
     this.sandbox.stub(templates, 'uiApp').returns('gitUrl');
+
+    const statSyncStub = this.sandbox.stub(fs, 'statSync');
+    statSyncStub.withArgs('./ui-test/__appDir__').returns({ isDirectory() { return true; } });
+    statSyncStub.withArgs('./ui-test/__appName__.js').returns({ isDirectory() { return false; } });
+    statSyncStub.withArgs('./ui-test/__appDir__/__appDir__').returns({ isDirectory() { return false; } });
+    statSyncStub.withArgs('./ui-test/__appDir__/__appName__.js').returns({ isDirectory() { return false; } });
   });
 
   it('calls appDefaults with expected responses', function (done) {
-    const result = createApp.appDefaults('test', 'test description');
+    const result = createApp.appDefaults('ui-test', 'test description');
 
     expect(result.uiAppName).to.eq('ui-test');
     expect(result.appDescription).to.eq('test description');
